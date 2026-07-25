8cdb0cec2c3d5e575abb2ae81886acf6ed8dd0ed
Olutayo Durodola <olutayodurodola@yahoo.com>
2026-07-25 11:26:36 +0100

    Search logic

--- searchlogic.py
+++ searchlogic.py
@@ -1,25 +1,4 @@
-print("Hello, World!");
-print("Another line");
-print(5 + 45);
-print("This is a test", 67, "and this is another test with a number", 89 );
-
-happy = True;
-
-if happy:
-    print("I am happy");
-elif not happy:
-    print("I am not happy");
-print("This is the end of the program");
-
-
-friends = ["Alice", "Bob", "Charlie", "David", "Eve"];
-for friend in friends:
-    print(f"Hello, {friend}!", end=" " );
-print();
-print("This is the end of the loop");
-
-
-#Forms of nested loops
+#Search logic implemented
 languages = ["Python", "Java", "C++", "C#", "JavaScript", "Go", "Rust", "C#", "Ruby", "Swift", "Kotlin", "PHP", "Perl", "C#",  "Scala", "Haskell", "Lua", "Dart", "Elixir", "Clojure", "F#", "R", "MATLAB", "Julia", "TypeScript", "Shell", "PowerShell", "Objective-C", "Visual Basic", "Assembly", "Fortran", "COBOL", "Ada", "Lisp"];
 
 
